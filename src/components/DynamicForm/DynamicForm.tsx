cb795c93f57990ae3c203fae1476a39783345566
24Nehak <41726371+24Nehak@users.noreply.github.com>
2022-06-21 16:14:15 +0530

    first chnage

---
+++ src/components/DynamicForm/DynamicForm.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const DynamicForm = () => {
+    const [studName, setStudName] = useState();
+    const handleChange = (e: any) =>{
+        console.log(e.target.value);
+    }
+  return (
+    <div>
+        <form>
+            <input type="number" value="" name="" onChange={handleChange}/>
+        </form>
+    </div>
+  )
+}
+
+export default DynamicForm
